f2fc2e7394cd5191d99596e8b1bd166fccef0cdb
OmarAshqar <o.ashqar@outlook.com>
2017-12-02 22:49:37 +0000

    cleanup

--- testingAudio.cpp
+++ testingAudio.cpp
@@ -3,8 +3,12 @@
 #include "lib/portaudio.h"
 
 using namespace std;
+typedef int PaStreamCallback(const void *input, void *output, unsigned long frameCount, const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags, void *userData){
 
+}
 int main() {
-    
+    err = Pa_Initialize();
+
+    err = Pa_Terminate();
     return 0;
 }
